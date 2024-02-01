b1032fb35bba42656e0096743ace8dd2a169eace
miyamoto1220 <miyamoto20031220@gmail.com>
2024-02-01 15:34:46 +0900

    デプロイの確認

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,14 +1,17 @@
+import Link from "next/link.js";
 import { client } from "../../libs/client.js"
-import styles from '../../src/app/Home.module.scss';
+import styles from "../../styles/Home.module.scss";
 
 //SSG
 export const getStaticProps = async (context) => {
    try {
       const id = context.params.id;
-      const data = await client.get({ endpoint: "blog", contentId: id });
-
+      const data = await client.get({
+         endpoint: "blog",
+         queries: { limit: 100 },
+         contentId: id
+      });
       console.log("Fetched data:", data);
-
       return {
          props: {
             blog: data,
@@ -23,9 +26,12 @@ export const getStaticProps = async (context) => {
       };
    }
 };
-export const getStaticPaths = async () => {
-   const data = await client.get({ endpoint: "blog" })
 
+export const getStaticPaths = async () => {
+   const data = await client.get({
+      endpoint: "blog",
+      queries: { limit: 100 },
+   })
    const paths = data.contents.map((content) => `/blog/${content.id}`);
    return {
       paths,
@@ -33,14 +39,45 @@ export const getStaticPaths = async () => {
    };
 };
 
+// formatDate 関数をコンポーネントの外に定義
+//日時だけを表示する
+const formatDate = (dateString) => {
+   const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+   const formattedDate = new Date(dateString).toLocaleDateString('ja-JP', options);
+   return formattedDate;
+};
+
 export default function BlogId({ blog }) {
+   console.log(blog);
    return (
-      <main className={styles.main}>
-         <h1 className={styles.title}>{blog.title}</h1>
-         <p className={styles.publishedAt}>{blog.publishedAt}</p>
-         <div dangerouslySetInnerHTML={{ __html: `${blog.body}` }}
-            className={styles.post}>
-         </div>
-      </main>
+      <div className={styles.body}>
+         <main className={styles.main}>
+            <p className={styles.title}>{blog.title}</p>
+            <div className={styles.day}>
+               <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" height="1em"
+                  viewBox="0 0 512 512">
+                  <path
+                     d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
+               </svg>
+               <p className={styles.article_day}>{formatDate(blog.day)}</p>
+            </div>
+            {blog.mainImage && (
+               <img className={styles.main_single_img}
+                  src={blog.mainImage.url}
+                  alt={blog.mainImage.alt}
+               />
+            )}
+            <div dangerouslySetInnerHTML={{ __html: `${blog.body}` }}
+               className={styles.post}>
+            </div>
+            <div className={styles.post_link}>
+               <div className={styles.top}>
+                  <Link href={`/`}>
+                     <p>topへ戻る</p>
+                  </Link>
+               </div>
+            </div>
+         </main>
+      </div>
    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { client } from "../libs/client";
 import Link from "next/link";
-import styles from "../src/app/Home.module.scss";
+import styles from "../styles/Home.module.scss";
 
 //SSG
 export const getStaticProps = async () => {
-   const data = await client.get({ endpoint: "blog" });
+
+   const data = await client.get({
+      endpoint: "blog",
+      queries: { limit: 100 },
+   });
    console.log(data);
    return {
       props: {
@@ -13,31 +17,48 @@ export const getStaticProps = async () => {
    }
 }
 
+// formatDate 関数をコンポーネントの外に定義
+const formatDate = (dateString) => {
+   const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+   const formattedDate = new Date(dateString).toLocaleDateString('ja-JP', options);
+   return formattedDate;
+};
+
 export default function Home({ blog }) {
+   console.log(blog);
    return (
-      <div>
-         <div>
-            <ul>
-               <li>aaa</li>
-               <li>iii</li>
-            </ul>
-         </div>
-         <div>
-            {blog.map((blog) => (
-               <li key={blog.id}>
-                  <Link href={`blog/${blog.id}`}>
-                     {blog.title}
-                  </Link>
-                  {blogItem.mainImage && (
-                     // メイン画像が存在する場合は表示
-                     <img
-                        src={blogItem.mainImage.url}
-                        alt={blogItem.mainImage.alt}
-                        className={styles.mainImage}
-                     />
-                  )}
-               </li>
-            ))}
+      <div className={styles.body}>
+         <div className={styles.inner}>
+            <div className={styles.article}>
+
+               {blog.map((blogItem) => (
+                  <div className={styles.content} key={blogItem.id}>
+                     <Link href={`/blog/${blogItem.id}`} passHref>
+                        <div className={styles.flex}>
+                           {blogItem.mainImage && (
+                              <img className={styles.main_img}
+                                 src={blogItem.mainImage.url}
+                                 alt={blogItem.mainImage.alt}
+                              />
+                           )}
+                           <div className={styles.article_text}>
+                              <p className={styles.article_title}>{blogItem.title}</p>
+                              <div className={styles.day}>
+                                 <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" height="1em"
+                                    viewBox="0 0 512 512">
+                                    <path
+                                       d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
+                                 </svg>
+                                 <p className={styles.article_day}>{formatDate(blogItem.day)}</p>
+                              </div>
+                           </div>
+
+                        </div>
+                     </Link>
+                  </div>
+               ))}
+
+            </div>
          </div>
       </div>
    );
